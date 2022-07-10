29b22db4df565ff9848b9bed77a568caf239e2b6
Naveen Malik <nmalik@redhat.com>
2022-07-10 18:29:28 -0400

    AddViews takes input paramaters
    Added parameter so you can create process icons to combine anything you want.

--- src/js/astroninjaus_AddViews.js
+++ src/js/astroninjaus_AddViews.js
@@ -1,35 +1,48 @@
+#include <pjsr/StdButton.jsh>
+#include <pjsr/StdIcon.jsh>
+#include "astroninjaus_Common.js"
+
 #feature-id  astroninja.us > Add Views
 
 #define ID                 "AddViews"
 #define TITLE              "Add Views"
-#define VERSION            "0.0.1"
+#define VERSION            "0.1.1"
 #define AUTHOR             "Naveen Malik"
 
-#feature-info adds all "$T[0-9]+" images to the applied image
-
+#feature-info adds all images matching the given input regex
 
 function mainAddViews() {
-    var window = ImageWindow.activeWindow;
-    if ( !window )
-    {
-       (new MessageBox( "There is no active image window!",
-                        TITLE, StdIcon_Error, StdButton_Ok )).execute();
-       return;
-    }
+   if (!Parameters.has("input_view_match")) {
+      (new MessageBox( "No 'input_view_match' parameter provided.",
+         TITLE, StdIcon_Error, StdButton_Ok )).execute();
+      return;
+   }
+   if (!Parameters.has("output_view_name")) {
+      (new MessageBox( "No 'output_view_name' parameter provided.",
+         TITLE, StdIcon_Error, StdButton_Ok )).execute();
+      return;
+   }
 
-    var baseId = window.currentView.id;
+   var input_view_match = Parameters.getString("input_view_match");
+   var output_view_name = Parameters.getString("output_view_name");
 
-    var PM = new PixelMath;
-    //PM.rescale = true;
+   var windows = ImageWindow.windows;
 
-    var windows = ImageWindow.windows;
-    for (var i in windows) {
-       if (windows[i].mainView.id.startsWith(baseId) && 
-            windows[i].mainView.id != baseId) {
-         PM.expression = format("$T + %s", windows[i].mainView.id);
-         PM.executeOn(window.currentView)
-       }
-    }
+   // create the output view
+   var outputWindow = cloneView(windows[0].mainView, output_view_name);
+   var PM = new PixelMath;
+	PM.expression = "0"
+	PM.executeOn(outputWindow.mainView)
+   outputWindow.show();
+
+   for (var i in windows) {
+      var x = windows[i].mainView.id.search(input_view_match)
+      if (x >= 0) {
+         PM.expression = format("$T + %s", windows[i].mainView.id)
+         PM.executeOn(outputWindow.mainView)
+         windows[i].forceClose()
+      }
+   }
 }
 
 mainAddViews();
