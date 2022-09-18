ea2399546ecc8dae2a4aed7ae51c0dec6b798e1f
Naveen Malik <nmalik@redhat.com>
2022-09-18 11:00:17 -0400

    Better matching stars image.
    Tries to find an exact match first then partial match.  If more than one partial match are found it reports an error.

--- src/js/astroninjaus_AddStars.js
+++ src/js/astroninjaus_AddStars.js
@@ -10,6 +10,8 @@
 
 #feature-info adds image "$T_stars" into active view
 
+#include "astroninjaus_Common.js"
+
 function mainAddStars() {
     // auto stretches the current active view using ez scripts
     var window = ImageWindow.activeWindow;
@@ -39,15 +41,15 @@ function mainAddStars() {
         linearize = true
     }
 
-    Console.warningln("linearize="+linearize)
 	Console.show();
-	Console.writeln("Adding stars back into image...")
+	Console.writeln("Adding stars back into image.")
 
     var stars
     var starsViewId = ""
     var starsViewIdSearch = format("%s_stars", window.currentView.id)
-
+    
     // try to find exact match
+    Console.writeln("Trying: exact match")
     var allWindows = ImageWindow.windows;
     for (var i in allWindows) {
         if (allWindows[i].mainView.id == starsViewIdSearch) {
@@ -62,6 +64,7 @@ function mainAddStars() {
     if (starsViewId == "") {
         // did not find exact match. try to find a partial match.
         // if more than one is found we cannot proceed
+        Console.writeln("Trying: partial match")
         found_count = 0
         for (var i in allWindows) {
             if (allWindows[i].mainView.id.contains(starsViewIdSearch)) {
@@ -73,7 +76,7 @@ function mainAddStars() {
         }
         if (found_count == 1) {
             Console.writeln(format("Single view partial match found: %s", starsViewId))
-        } else {
+        } else if (found_count > 1) {
             Console.warningln("Multiple partial matches found, cannot proceed!")
             stars = null
             starsViewId = ""
@@ -85,14 +88,52 @@ function mainAddStars() {
         }
     }
 
-    if (starsViewId != "") {
+    if (starsViewId == "" && starsViewIdSearch.contains("_clone")) {
+        // didn't find exact or partial match, but it's a clone.
+        // try without _clone
+        // if more than one is found we cannot proceed
+        starsViewIdSearch = starsViewIdSearch.replace("_clone", "")
+        Console.writeln("Trying: partial match excluding '_clone' " + starsViewIdSearch)
+        found_count = 0
+        for (var i in allWindows) {
+            if (allWindows[i].mainView.id.contains(starsViewIdSearch)) {
+                // found partial match.
+                stars = allWindows[i]
+                starsViewId = allWindows[i].mainView.id
+                found_count++
+            }
+        }
+        if (found_count == 1) {
+            Console.writeln(format("Single view partial match found: %s", starsViewId))
+        } else if (found_count > 1) {
+            Console.warningln("Multiple partial matches found, cannot proceed!")
+            stars = null
+            starsViewId = ""
+            for (var i in allWindows) {
+                if (allWindows[i].mainView.id.contains(starsViewIdSearch)) {
+                    Console.warningln(format("    %s", allWindows[i].mainView.id))
+                }
+            }
+        }
+    }
+
+
+    if (starsViewId == "") {
+        Console.warningln("No matches found, cannot proceed!")
+        stars = null
+        starsViewId = ""
+    } else if (starsViewId != "") {
         var old_mask = window.maskEnabled;
+
+        // get the _stars window
+        var _stars = cloneView(stars.mainView, format("__%s", stars.mainView.id))
 
         if (remove_mask) {
             window.maskEnabled = false
         }
 
         if (linearize) {
+            window.maskEnabled = false
             var P = new HistogramTransformation;
             P.H = [ // c0, m, c1, r0, r1
                [0.00000000, 0.50000000, 1.00000000, 0.00000000, 1.00000000],
@@ -102,14 +143,18 @@ function mainAddStars() {
                [0.00000000, 0.50000000, 1.00000000, 0.00000000, 1.00000000]
             ];
             P.executeOn(window.currentView)
-            P.executeOn(stars.mainView)
+            P.executeOn(_stars.mainView)
+            if (!remove_mask) {
+                window.maskEnabled = old_mask
+            }
         }
 
         var PM = new PixelMath;
-        PM.expression = format("%s + %s", window.currentView.id, starsViewId)
+        PM.expression = format("%s + %s", window.currentView.id, _stars.mainView.id)
         PM.executeOn(window.currentView)
 
         if (linearize) {
+            window.maskEnabled = false
             var P = new HistogramTransformation;
             P.H = [ // c0, m, c1, r0, r1
                [0.00000000, 0.50000000, 1.00000000, 0.00000000, 1.00000000],
@@ -119,8 +164,12 @@ function mainAddStars() {
                [0.00000000, 0.50000000, 1.00000000, 0.00000000, 1.00000000]
             ];
             P.executeOn(window.currentView)
-            stars.undo()
+            if (!remove_mask) {
+                window.maskEnabled = old_mask
+            }
         }
+
+        _stars.forceClose()
 
         if (remove_mask) {
             window.maskEnabled = old_mask
